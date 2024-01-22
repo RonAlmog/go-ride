69c994298ed3f15ed8734b8ff1a129301d68a646
Ron Almog <ron8132@gmail.com>
2024-01-22 09:15:37 -0500

    change font to raleway

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      test
+      <UserButton afterSignOutUrl="/" />
+      <h2 className="text-2xl font-semibold">Title here</h2>
+      <p>Test page</p>
     </main>
   );
 }
