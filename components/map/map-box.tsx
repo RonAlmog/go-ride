ce792cd36b41a2da96517d8e664d5cc33d2d3635
Ron Almog <ron8132@gmail.com>
2024-01-25 09:50:47 -0500

    fly to

--- components/map/map-box.tsx
+++ components/map/map-box.tsx
@@ -1,20 +1,46 @@
 "use client";
 import { UseLocationContext } from "@/context/use-location-context";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import Map, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Markers from "./markers";
+import { SourceCoordinatesContext } from "@/context/source-context";
+import { DestinationCoordinatesContext } from "@/context/destination-context";
 
 type Props = {};
 
 const MapBox = (props: Props) => {
   const { userLocation, setUserLocation } = useContext(UseLocationContext);
+  const { sourceCoordinates, setSourceCoordinates } = useContext(
+    SourceCoordinatesContext
+  );
+  const { destinationCoordinates, setDestinationCoordinates } = useContext(
+    DestinationCoordinatesContext
+  );
+  const mapRef = useRef<any>();
+  useEffect(() => {
+    if (sourceCoordinates) {
+      mapRef.current?.flyTo({
+        center: [sourceCoordinates.lon, sourceCoordinates.lat],
+        duration: 2500,
+      });
+    }
+  }, [sourceCoordinates]);
+  useEffect(() => {
+    if (destinationCoordinates) {
+      mapRef.current?.flyTo({
+        center: [destinationCoordinates.lon, destinationCoordinates.lat],
+        duration: 2500,
+      });
+    }
+  }, [destinationCoordinates]);
   return (
     <div className="p-5">
       <h2 className="text-lg font-semibold">Map</h2>
       <div className="rounded-lg overflow-hidden">
         {userLocation ? (
           <Map
+            ref={mapRef}
             mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
             initialViewState={{
               longitude: userLocation?.lon,
